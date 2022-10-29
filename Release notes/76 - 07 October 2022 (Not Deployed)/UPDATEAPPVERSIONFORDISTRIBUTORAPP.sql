c5d8c28c8992c991d07e2afbbbd247de55be4499
samitkumarsinha <samitkumarsinha@gmail.com>
2022-10-29 23:06:19 +0530

    init

---
+++ Release notes/76 - 07 October 2022 (Not Deployed)/UPDATEAPPVERSIONFORDISTRIBUTORAPP.sql
@@ -0,0 +1,3 @@
+update app_version set build_min_no = 34 where platform =1 limit 1;
+
+update app_version set build_min_no = 113 where platform =2 limit 1;
---
+++ Release notes/76 - 07 October 2022 (Not Deployed)/UPDATEAPPVERSIONFORINVESTORAPP.sql
@@ -0,0 +1,4 @@
+update app_version set build_min_no = 73 where platform =1 limit 1;
+
+update app_version set build_min_no = 179 where platform =2 limit 1;
+
